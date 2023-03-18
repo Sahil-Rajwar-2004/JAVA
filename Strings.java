1b1535f6fd0afbfd2c06ea6412922960bb939c92
Sahil-Rajwar-2004 <sahilrajwar2004@gmail.com>
2023-03-18 20:52:46 +0530

    strings

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings{
+	public static void main(String[] args){
+		String x = "sahil";
+		String y = "sahil";
+		String a = new String("Sahil");
+		String b = new String("Sahil");
+		System.out.println(x==y);
+		System.out.println(a==b);
+	}
+}
+
