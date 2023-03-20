8a38e99b8322afa09eb1e08a7c8fb6ff4896e250
Sahil-Rajwar-2004 <sahilrajwar2004@gmail.com>
2023-03-20 11:02:51 +0530

    Numbers

---
+++ Number.java
@@ -0,0 +1,9 @@
+public class Number{
+	public static void main(String[] args){
+		Integer num = 5;
+		Integer x = new Integer(5);
+		int y = 5;
+		System.out.println(num == x);
+		System.out.println(num == y);
+	}
+}
